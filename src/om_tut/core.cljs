28426aa3948a2c5cc74188eb8fc3ca2598c22f97
Bryan Garza <brygarza@gmail.com>
2014-09-23 23:43:04 -0700

    Intial commit and first Om component

---
+++ src/om_tut/core.cljs
@@ -0,0 +1,42 @@
+(ns om-tut.core
+  (:require [om.core :as om :include-macros true]
+            [om.dom :as dom :include-macros true]))
+
+(enable-console-print!)
+
+(def app-state
+  (atom
+    {:contacts
+     [{:first "Ben" :last "Bitdiddle" :email "benb@mit.edu"}
+      {:first "Alyssa" :middle-initial "P" :last "Hacker" :email "aphacker@mit.edu"}
+      {:first "Eva" :middle "Lu" :last "Ator" :email "eval@mit.edu"}
+      {:first "Louis" :last "Reasoner" :email "prolog@mit.edu"}
+      {:first "Cy" :middle-initial "D" :last "Effect" :email "bugs@mit.edu"}
+      {:first "Lem" :middle-initial "E" :last "Tweakit" :email "morebugs@mit.edu"}
+      ]}))
+
+(defn middle-name [{:keys [middle middle-initial]}]
+  (cond
+    middle (str " " middle)
+    middle-initial (str " " middle-initial ".")))
+
+(defn display-name [{:keys [first last] :as contact}]
+  (str last ", " first (middle-name contact)))
+
+(defn contact-view [contact owner]
+  (reify
+    om/IRender
+    (render [this]
+      (dom/li nil (display-name contact)))))
+
+(defn contacts-view [app owner]
+  (reify
+    om/IRender
+    (render [this]
+      (dom/div nil
+               (dom/h2 nil "Contact list")
+               (apply dom/ul nil
+                      (om/build-all contact-view (:contacts app)))))))
+
+(om/root contacts-view app-state
+         {:target (. js/document (getElementById "contacts"))})
